9df15a478928692de4cab6a87ef13339462f1daa
EmmanuelCruz <emmanuel_cruzh@ciencias.unam.mx>
2021-08-22 18:24:42 -0500

    Transición de background automático

--- src/App.vue
+++ src/App.vue
@@ -26,14 +26,101 @@ export default {
 
 <style scoped>
 #app-container{
-  background: linear-gradient(
-      rgba(0, 0, 0, 0.7),
-      rgba(0, 0, 0, 0.7)
-    ),
-    url('./assets/background2.jpg') no-repeat center center fixed !important;
   background-size: cover;
 
   /* font-family: 'Roboto', 'Open Sans', serif !important;
   font-size: 22px !important; */
+  
+  animation: bg_transition 34s infinite;
 }
+
+/* Animación de transición de imágenes automático. */
+@keyframes bg_transition {
+	0% {
+		background: linear-gradient(
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0.7)
+    ),
+    url('./assets/background1.jpg') no-repeat center center fixed;
+  }
+  11% {
+		background: linear-gradient(
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0.7)
+    ),
+    url('./assets/background1.jpg') no-repeat center center fixed;
+  }
+  17% {
+		background: linear-gradient(
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0.7)
+    ),
+    url('./assets/background2.jpg') no-repeat center center fixed;
+  }
+  28% {
+		background: linear-gradient(
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0.7)
+    ),
+    url('./assets/background2.jpg') no-repeat center center fixed;
+  }
+  34% {
+		background: linear-gradient(
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0.7)
+    ),
+    url('./assets/background3.jpg') no-repeat center center fixed;
+  }
+  45% {
+		background: linear-gradient(
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0.7)
+    ),
+    url('./assets/background3.jpg') no-repeat center center fixed;
+  }
+  51% {
+		background: linear-gradient(
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0.7)
+    ),
+    url('./assets/background4.jpg') no-repeat center center fixed;
+  }
+  62% {
+		background: linear-gradient(
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0.7)
+    ),
+    url('./assets/background4.jpg') no-repeat center center fixed;
+  }
+  70% {
+		background: linear-gradient(
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0.7)
+    ),
+    url('./assets/background5.jpg') no-repeat center center fixed;
+  }
+  82% {
+		background: linear-gradient(
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0.7)
+    ),
+    url('./assets/background5.jpg') no-repeat center center fixed;
+  }
+  88% {
+		background: linear-gradient(
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0.7)
+    ),
+    url('./assets/background6.jpg') no-repeat center center fixed;
+  }
+  100% {
+		background: linear-gradient(
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0.7)
+    ),
+    url('./assets/background6.jpg') no-repeat center center fixed;
+  }
+  
+}
+
 </style>
