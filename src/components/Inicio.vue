4c191bc8a9bc0b1f39ddebb96fabcec99a6f8b67
EmmanuelCruz <emmanuel_cruzh@ciencias.unam.mx>
2021-08-31 02:11:44 -0500

    Background

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -2,6 +2,8 @@
 <!-- El diseño principal consiste en un fondo fotográfico, el icono del proyecto, título y la descripción del proyecto -->
 
 <template>
+
+	<div id="animation-template">
   <v-container>
     <!-- Uso de filas para organizar -->
     <v-row class="text-justify">
@@ -29,6 +31,7 @@
       </v-col>
     </v-row>
   </v-container>
+  </div>
 </template>
 
 
@@ -41,3 +44,104 @@
     }),
   }
 </script>
+
+<style scope>
+#animation-template{
+  background-size: cover;
+
+  /* font-family: 'Roboto', 'Open Sans', serif !important;
+  font-size: 22px !important; */
+  
+  animation: bg_transition 34s infinite;
+}
+
+/* Animación de transición de imágenes automático. */
+@keyframes bg_transition {
+	0% {
+		background: linear-gradient(
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0.7)
+    ),
+    url('../assets/background1.jpg') no-repeat center center fixed;
+  }
+  11% {
+		background: linear-gradient(
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0.7)
+    ),
+    url('../assets/background1.jpg') no-repeat center center fixed;
+  }
+  17% {
+		background: linear-gradient(
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0.7)
+    ),
+    url('../assets/background2.jpg') no-repeat center center fixed;
+  }
+  28% {
+		background: linear-gradient(
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0.7)
+    ),
+    url('../assets/background2.jpg') no-repeat center center fixed;
+  }
+  34% {
+		background: linear-gradient(
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0.7)
+    ),
+    url('../assets/background3.jpg') no-repeat center center fixed;
+  }
+  45% {
+		background: linear-gradient(
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0.7)
+    ),
+    url('../assets/background3.jpg') no-repeat center center fixed;
+  }
+  51% {
+		background: linear-gradient(
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0.7)
+    ),
+    url('../assets/background4.jpg') no-repeat center center fixed;
+  }
+  62% {
+		background: linear-gradient(
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0.7)
+    ),
+    url('../assets/background4.jpg') no-repeat center center fixed;
+  }
+  70% {
+		background: linear-gradient(
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0.7)
+    ),
+    url('../assets/background5.jpg') no-repeat center center fixed;
+  }
+  82% {
+		background: linear-gradient(
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0.7)
+    ),
+    url('../assets/background5.jpg') no-repeat center center fixed;
+  }
+  88% {
+		background: linear-gradient(
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0.7)
+    ),
+    url('../assets/background6.jpg') no-repeat center center fixed;
+  }
+  100% {
+		background: linear-gradient(
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0.7)
+    ),
+    url('../assets/background6.jpg') no-repeat center center fixed;
+  }
+  
+}
+
+</style>
