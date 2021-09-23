c0d703961000babfc6820ee1affc605fba30f57d
EmmanuelCruz <emmanuel_cruzh@ciencias.unam.mx>
2021-09-23 13:30:09 -0500

    Modificación y nuevos hints

---
+++ src/components/Hint.vue
@@ -0,0 +1,21 @@
+<!-- Componente para un hint -->
+
+<template>
+    <v-tooltip top>
+        <template v-slot:activator="{ on, attrs }">
+            <v-icon small v-bind="attrs" v-on="on">
+                mdi-information
+            </v-icon>
+        </template>
+        <span>{{ hint }}</span>
+    </v-tooltip>
+</template>
+
+<script>
+export default {
+  name: 'Hint',
+  props: { // Propiedades (sólo lectura) o argumentos para el componente
+    hint: { type: String } // Mensaje contenido en el hint
+  }
+}
+</script>
--- src/components/VideoInfo.vue
+++ src/components/VideoInfo.vue
@@ -45,116 +45,53 @@
               <v-card-text>
                 <p>
                   <span class="font-weight-bold">Código de referencia</span>
-                    <v-tooltip top>
-                      <template v-slot:activator="{ on, attrs }">
-                        <v-icon small v-bind="attrs" v-on="on">
-                          mdi-information
-                        </v-icon>
-                      </template>
-                      <span>Código alfanumérico separado por guiones. Ejemplo: MXIM-AV-2-3-1-2</span>
-                    </v-tooltip>
+                    <Hint hint="Código alfanumérico separado por guines. Ejemplo: MXIM-AV-2-3-1"></Hint>
                     {{ video.identificacion.codigoReferencia }}
-
                 </p>
                 <p>
                   <span class="font-weight-bold">Título</span>
+                  <Hint hint="Título de la colección"></Hint>
                   {{ video.identificacion.titulo }}
                 </p>
                 <p>
                   <span class="font-weight-bold">Fecha</span>
+                  <Hint hint="Fecha en que se hizo el registro"></Hint>
                   {{ video.identificacion.fecha | date}}
-                  <v-tooltip top>
-                      <template v-slot:activator="{ on, attrs }">
-                        <v-icon small v-bind="attrs" v-on="on">
-                          mdi-information
-                        </v-icon>
-                      </template>
-                      <span>Fecha en que se hizo el registro</span>
-                    </v-tooltip>
                 </p>
                 <p>
                   <span class="font-weight-bold">País</span>
+                  <Hint hint="País o países de producción del registro en video"></Hint>
                   {{ video.identificacion.pais }}
-                  <v-tooltip top>
-                      <template v-slot:activator="{ on, attrs }">
-                        <v-icon small v-bind="attrs" v-on="on">
-                          mdi-information
-                        </v-icon>
-                      </template>
-                      <span>País o países de producción del registro en video</span>
-                    </v-tooltip>
                 </p>
                 <p>
                   <span class="font-weight-bold">Lugar de registro</span>
+                  <Hint hint="Nombre del lugar o lugares, donde se llevó a cabo el registro, partiendo de lo particular a lo general"></Hint>
                   {{ video.identificacion.lugar }}
-                  <v-tooltip top>
-                      <template v-slot:activator="{ on, attrs }">
-                        <v-icon small v-bind="attrs" v-on="on">
-                          mdi-information
-                        </v-icon>
-                      </template>
-                      <span>Nombre del lugar o lugares, donde se llevó a cabo el registro, partiendo de lo particular a lo general</span>
-                    </v-tooltip>
                 </p>
                 <p>
                   <span class="font-weight-bold">Duración</span>
+                  <Hint hint="Se consigna la duración del registro en minutos"></Hint>
                   {{ video.identificacion.duracion }} minutos
-                  <v-tooltip top>
-                      <template v-slot:activator="{ on, attrs }">
-                        <v-icon small v-bind="attrs" v-on="on">
-                          mdi-information
-                        </v-icon>
-                      </template>
-                      <span>Se consigna la duración del registro en minutos</span>
-                    </v-tooltip>
                 </p>
                 <p>
                   <span class="font-weight-bold">Persona entrevistada</span>
+                  <Hint hint="Persona que entabla un diálogo con la persona que dirige la entrevista"></Hint>
                   {{ video.identificacion.personasEntrevistadas }}
-                  <v-tooltip top>
-                      <template v-slot:activator="{ on, attrs }">
-                        <v-icon small v-bind="attrs" v-on="on">
-                          mdi-information
-                        </v-icon>
-                      </template>
-                      <span>Persona que entabla un diálogo con la persona que dirige la entrevista</span>
-                    </v-tooltip>
                 </p>
                 <p>
                   <span class="font-weight-bold">Persona que entrevista</span>
+                  <Hint hint="Persona encargada de realizar la entrevista"></Hint>
                   {{ video.identificacion.entrevistador }}
-                  <v-tooltip top>
-                      <template v-slot:activator="{ on, attrs }">
-                        <v-icon small v-bind="attrs" v-on="on">
-                          mdi-information
-                        </v-icon>
-                      </template>
-                      <span>Persona encargada de realizar la entrevista</span>
-                    </v-tooltip>
                 </p>
                 <p>
                   <span class="font-weight-bold">Cámara</span>
+                  <Hint hint="Persona encargada de operar la cámara de video"></Hint>
                   {{ video.identificacion.camara }}
-                  <v-tooltip top>
-                    <template v-slot:activator="{ on, attrs }">
-                        <v-icon small v-bind="attrs" v-on="on">
-                          mdi-information
-                        </v-icon>
-                      </template>
-                    <span>Persona encargada de operar la cámara de video</span>
-                  </v-tooltip>
                 </p>
                 <p>
                   <span class="font-weight-bold">Asistente</span>
+                  <Hint hint="Persona o personas encargadas de apoyar en diversas labores técnicas"></Hint>
                   {{ video.identificacion.asistente }}
-                  <v-tooltip top>
-                    <template v-slot:activator="{ on, attrs }">
-                        <v-icon small v-bind="attrs" v-on="on">
-                          mdi-information
-                        </v-icon>
-                      </template>
-                    <span>Persona o personas encargadas de apoyar en diversas labores técnicas</span>
-                  </v-tooltip>
                 </p>
               </v-card-text>
             </v-card>
@@ -165,45 +102,24 @@
               <v-card-text>
                 <p>
                   <span class="font-weight-bold">Descripción general</span>
+                  <Hint hint="Contenido del material. Se describen los lugares, personas y/o acciones registradas, según tipos de plano, emplazamientos y movimientos"></Hint>
                   {{ video.contenidoEstructura.descripcionGeneral }}
-                  <v-tooltip top>
-                    <template v-slot:activator="{ on, attrs }">
-                        <v-icon small v-bind="attrs" v-on="on">
-                          mdi-information
-                        </v-icon>
-                      </template>
-                    <span>Contenido del material. Se describen los lugares, personas y/o acciones registradas, según tipos de plano, emplazamientos y movimientos</span>
-                  </v-tooltip>
                 </p>
                 <p>
                   <span class="font-weight-bold">Estructura formal</span>
+                  <Hint hint="Tipo de registro: grabación en campo, registro con entrevista o registro de materiales"></Hint>
                   {{ video.contenidoEstructura.estructuraFormal }}
                 </p>
                 <p>
                   <span class="font-weight-bold">Descriptor onomástico</span>
+                  <Hint hint="Nombres de las personas (nombre y apellido) que aparecen el registro"></Hint>
                   {{ video.contenidoEstructura.descriptorOnomastico }}
-                  <v-tooltip top>
-                    <template v-slot:activator="{ on, attrs }">
-                        <v-icon small v-bind="attrs" v-on="on">
-                          mdi-information
-                        </v-icon>
-                      </template>
-                    <span>Nombres de las personas (nombre y apellido) que aparecen el registro</span>
-                  </v-tooltip>
 
                 </p>
                 <p>
                   <span class="font-weight-bold">Descriptor toponímico</span>
+                  <Hint hint="Nombres de las localidades o sitios que aparecen en el registro"></Hint>
                   {{ video.contenidoEstructura.descriptorToponimico }}
-                  <v-tooltip top>
-                    <template v-slot:activator="{ on, attrs }">
-                        <v-icon small v-bind="attrs" v-on="on">
-                          mdi-information
-                        </v-icon>
-                      </template>
-                    <span>Nombres de las localidades o sitios que aparecen en el registro</span>
-                  </v-tooltip>
-
                 </p>
               </v-card-text>
             </v-card>
@@ -214,56 +130,48 @@
               <v-card-text>
                 <p>
                   <span class="font-weight-bold">Idioma original</span>
+                  <Hint hint="Idioma o idiomas originales del registro"></Hint>
                   {{ video.accesoUso.idiomaOriginal }}
-                  <v-tooltip top>
-                    <template v-slot:activator="{ on, attrs }">
-                        <v-icon small v-bind="attrs" v-on="on">
-                          mdi-information
-                        </v-icon>
-                      </template>
-                    <span>Idioma o idiomas originales del registro</span>
-                  </v-tooltip>
                 </p>
                 <p>
                   <span class="font-weight-bold">Soporte original</span>
+                  <Hint hint="Soporte del material: Betacam, Hi8, DVCAM, MiniDV o Archivo digital."></Hint>
                   {{ video.accesoUso.soporte }}
                 </p>
                 <p>
                   <span class="font-weight-bold">Sistema de grabación</span>
+                  <Hint hint="Sistema de grabación y reproducción de los registros: NTSC, PAL o SECAM."></Hint>
                   {{ video.accesoUso.sistemaGrabacion }}
                 </p>
                 <p>
                   <span class="font-weight-bold">Resolución de grabación</span>
+                  <Hint hint="Número de píxeles que puede mostrar el registro en video."></Hint>
                   {{ video.accesoUso.resolucionGrabacion }}
                 </p>
                 <p>
                   <span class="font-weight-bold">Formato de video digital</span>
+                  <Hint hint="Formato de archivo que almacena los datos del video digital en una computadora."></Hint>
                   {{ video.accesoUso.formatoVideoDigital }}
                 </p>
                 <p>
                   <span class="font-weight-bold">Número de casetes</span>
+                  <Hint hint="Número de casetes que conforman la entrevista."></Hint>
                   {{ video.accesoUso.numeroCasetes }}
                 </p>
                 <p>
                   <span class="font-weight-bold">Color</span>
+                  <Hint hint="Se consigna si el registro es a color o blanco y negro."></Hint>
                   {{ video.accesoUso.color }}
                 </p>
                 <p>
                   <span class="font-weight-bold">Audio</span>
+                  <Hint hint="Sistema de audio en que se encuentra la producción."></Hint>
                   {{ video.accesoUso.audio }}
                 </p>
                 <p>
                   <span class="font-weight-bold">Requisitos técnicos</span>
+                  <Hint hint="Equipo de reproducción o software que se requiere para visionar el material"></Hint>
                   {{ video.accesoUso.requisitosTecnicos }}
-                  <v-tooltip top>
-                    <template v-slot:activator="{ on, attrs }">
-                        <v-icon small v-bind="attrs" v-on="on">
-                          mdi-information
-                        </v-icon>
-                      </template>
-                    <span>Equipo de reproducción o software que se requiere para visionar el material</span>
-                  </v-tooltip>
-
                 </p>
               </v-card-text>
             </v-card>
@@ -274,28 +182,13 @@
               <v-card-text>
                 <p>
                   <span class="font-weight-bold">Unidades de descripción relacionadas</span>
+                  <Hint hint="Relación que existe entre dos o más unidades de la misma colección a partir de coincidencias temáticas o conceptuales"></Hint>
                   {{ video.documentacionAsociada.unidadesDescripcionRelacionadas }}
-                  <v-tooltip top>
-                    <template v-slot:activator="{ on, attrs }">
-                        <v-icon small v-bind="attrs" v-on="on">
-                          mdi-information
-                        </v-icon>
-                      </template>
-                    <span>Relación que existe entre dos o más unidades de la misma colección a partir de coincidencias temáticas o conceptuales</span>
-                  </v-tooltip>
                 </p>
                 <p>
                   <span class="font-weight-bold">Documentos asociados</span>
+                  <Hint hint="Publicación o documento que tenga relación directa con la producción catalogada. La relación puede ser temática, autoral, etc."></Hint>
                   {{ video.documentacionAsociada.documentosAsociados }}
-                  <v-tooltip top>
-                    <template v-slot:activator="{ on, attrs }">
-                        <v-icon small v-bind="attrs" v-on="on">
-                          mdi-information
-                        </v-icon>
-                      </template>
-                    <span>Publicación o documento que tenga relación directa con la producción catalogada. La relación puede ser temática, autoral, etc.</span>
-                  </v-tooltip>
-
                 </p>
               </v-card-text>
             </v-card>
@@ -305,15 +198,8 @@
               <v-card-text>
                 <p>
                   <span class="font-weight-bold">Notas</span>
+                  <Hint hint="Información que se considere relevante y que no pudo ser indexada en algún otro campo"></Hint>
                   {{ video.notas.notas }}
-                  <v-tooltip top>
-                    <template v-slot:activator="{ on, attrs }">
-                        <v-icon small v-bind="attrs" v-on="on">
-                          mdi-information
-                        </v-icon>
-                      </template>
-                    <span>Información que se considere relevante y que no pudo ser indexada en algún otro campo</span>
-                  </v-tooltip>
                 </p>
               </v-card-text>
             </v-card>
@@ -324,15 +210,8 @@
               <v-card-text>
                 <p>
                   <span class="font-weight-bold">Nombre del archivero</span>
+                  <Hint hint="Nombre completo de la persona que elaboró la ficha de la unidad"></Hint>
                   {{ video.controlDescripcion.nombreArchivero }}
-                  <v-tooltip top>
-                    <template v-slot:activator="{ on, attrs }">
-                        <v-icon small v-bind="attrs" v-on="on">
-                          mdi-information
-                        </v-icon>
-                      </template>
-                    <span>Nombre completo de la persona que elaboró la ficha de la unidad</span>
-                  </v-tooltip>
                 </p>
                 <!-- Nota: Este campo podría cambiar o eliminarse en el futuro -->
                 <!-- <p>
@@ -341,27 +220,13 @@
                 </p> -->
                 <p>
                   <span class="font-weight-bold">Fecha de descripción</span>
+                  <Hint hint="Fecha en que se elaboró la ficha de la unidad"></Hint>
                   {{ video.controlDescripcion.fechaDescripcion | date}}
-                  <v-tooltip top>
-                    <template v-slot:activator="{ on, attrs }">
-                        <v-icon small v-bind="attrs" v-on="on">
-                          mdi-information
-                        </v-icon>
-                      </template>
-                    <span>Fecha en que se elaboró la ficha de la unidad</span>
-                  </v-tooltip>
                 </p>
                 <p>
                   <span class="font-weight-bold">Fecha de actualización</span>
+                  <Hint hint="Fecha de la última modificación a la ficha de la unidad"></Hint>
                   {{ video.controlDescripcion.fechaActualizacion | date}}
-                  <v-tooltip top>
-                    <template v-slot:activator="{ on, attrs }">
-                        <v-icon small v-bind="attrs" v-on="on">
-                          mdi-information
-                        </v-icon>
-                      </template>
-                    <span>Fecha de la última modificación a la ficha de la unidad</span>
-                  </v-tooltip>
                 </p>
               </v-card-text>
             </v-card>
@@ -396,8 +261,13 @@
 
 
 <script>
+import Hint from '@/components/Hint.vue'
+
 export default {
   name: 'VideoInfo',
+  components: {
+    Hint
+  },
   props: { // Propiedades (sólo lectura) o argumentos para el componente
     video: { type: Object } // archivo audiovisual organizado por áreas
   }
