5c2570f5ab58aa702b81f38b7bddc516df29ab69
EmmanuelCruz <emmanuel_cruzh@ciencias.unam.mx>
2022-07-28 21:07:27 -0500

    Mensaje para contraseña incorrecta

--- src/views/authentication/Login.vue
+++ src/views/authentication/Login.vue
@@ -1,10 +1,24 @@
 <template>
-  <v-form ref="formulario" v-model="valid" lazy-validation v-on:submit.prevent="onSubmit">
-    <v-text-field v-model="usuario.username" :rules="rules.username" label="Nombre de usuario" required ></v-text-field>
-    <v-text-field v-model="usuario.password" :rules="rules.password" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" :type="show1 ? 'text' : 'password'" label="Contraseña" required></v-text-field>
+  <div>
+    <!-- En caso de contraseña incorrecta -->
+    <v-alert v-model="incorrectPassword" dismissible type="error">
+      <v-row align-item="center">
+        <v-col class="grow">
+          Contraseña incorrecta
+        </v-col>
+        <v-col class="shrink">
+          <v-btn href="/home">Ir a inicio</v-btn> <!-- TODO Incorrect URL redirect. Use <router-link> instead -->
+        </v-col>
+      </v-row>
+    </v-alert>
 
-    <v-btn type="submit" :disabled="!valid" color="primary" class="mr-4"> Iniciar sesión </v-btn>
-  </v-form>
+    <v-form ref="formulario" v-model="valid" lazy-validation v-on:submit.prevent="onSubmit">
+      <v-text-field v-model="usuario.username" :rules="rules.username" label="Nombre de usuario" required ></v-text-field>
+      <v-text-field v-model="usuario.password" :rules="rules.password" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" :type="show1 ? 'text' : 'password'" label="Contraseña" required></v-text-field>
+  
+      <v-btn type="submit" :disabled="!valid" color="primary" class="mr-4"> Iniciar sesión </v-btn>
+    </v-form>
+  </div>
 </template>
 
 <script>
@@ -27,7 +41,9 @@ export default{
         password: [
           value => !!value || 'La contraseña es necesaria',
         ],
-      }
+      },
+      // Determina si la contraseña es incorrecta
+      incorrectPassword: false,
     };
   },
   methods: {
@@ -36,8 +52,12 @@ export default{
       if(!this.$refs.formulario.validate()) // Si el formulario no es válido
         return;
       const user = this.usuario;
-      await auth.login(user);
-      this.$router.push({name: 'home'});
+      try {
+        await auth.login(user);
+        this.$router.push({name: 'home'});
+      } catch (error) {
+        this.incorrectPassword = true
+      }
     }
   }
 }
